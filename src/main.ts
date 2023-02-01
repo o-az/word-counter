687993f679ba111d65eed6593065ca9db3e1c002
Omar Aziz <github@omaraziz.dev>
2023-01-31 23:29:17 -0800

    hee

--- src/main.ts
+++ src/main.ts
@@ -4,21 +4,18 @@ import { checkedQuerySelector } from './utilities';
 
 const app = checkedQuerySelector<HTMLDivElement>('#app');
 app.innerHTML = /* html */ `
-  <main>
-    <div class="text-3xl flex justify-evenly text-center mb-6">
-      <p>
-        words
-        <span id="word-count">0</span>
-      </p>
-      <button id="reset">reset</button>
-      <p>
-        characters
-        <span id="character-count">0</span>
-      </p>
-
-    </div>
-    <textarea class="text-white p-4 resize w-[600px] h-[300px] text-xl font-medium"></textarea>
-  </main>
+  <div class="container">
+    <p>
+      words
+      <span id="word-count">0</span>
+    </p>
+    <button id="reset">reset</button>
+    <p>
+      characters
+      <span id="character-count">0</span>
+    </p>
+  </div>
+  <textarea class="text-white p-4 resize w-[600px] h-[300px] text-xl font-medium"></textarea>
 `;
 
 const resetButton = checkedQuerySelector<HTMLButtonElement>('#reset');
